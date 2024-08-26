9bde0ae171b02b264eb8204cc49217ca9f7bc908
Lance Nelson <lancen04@gmail.com>
2024-08-26 17:50:06 -0400

    added .pgdump file to db/data

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start
+
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
